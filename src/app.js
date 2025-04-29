274953f2fcfdff76276f7aed4ad7b93f6632164e
Dannick010 <danmaggot2@gmail.com>
2025-04-29 13:52:27 -0300

    fix/PORT

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
-const port = process.env.port || 8081
+const port = process.env.PORT || 8081
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
